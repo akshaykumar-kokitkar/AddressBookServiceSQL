bf84434dc15b54dd635c2791b48ec38cd963ed0f
ak47 <kokitkarak@gmail.com>
2022-01-25 10:54:24 +0530

    ability to create address book table with user details input

--- AddressBookSQL.sql
+++ AddressBookSQL.sql
@@ -1,4 +1,18 @@
-create database addressbook_serviceDB
 
 use addressbook_serviceDB;
 select DB_NAME();
+
+create table address_book
+(
+ID int identity(1,1) primary key,
+firstname varchar(100) NOT NULL,
+lastnane varchar(100) NOT NULL,
+address varchar(250) NOT NULL,
+city varchar(100) NOT NULL,
+state varchar(100) NOT NULL,
+zip varchar(6) NOT NULL,
+phone varchar(12) NOT NULL,
+email varchar(100) NOT NULL
+);
+
+select * from address_book;
